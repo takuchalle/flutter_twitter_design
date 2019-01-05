a4b6683385a577c47f83bfdebe79e8a329d442a8
Takuya OHASHI <tky.ohashi@gmail.com>
2019-01-05 12:03:33 +0900

    Add timeline

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -4,6 +4,50 @@ class MyHomeScreen extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Text('Index 0: Home');
+    return ListView(
+      shrinkWrap: true,
+      padding: const EdgeInsets.all(20.0),
+      children: <Widget>[
+        Tweet(),
+        Tweet(),
+        Tweet(),
+        Tweet(),
+      ],
+    );
+  }
+}
+
+class TweetLink extends StatelessWidget {
+  final _iconSize = 15.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.all(4),
+        child: Row(children: [
+          Expanded(child: Icon(Icons.mode_comment, size: _iconSize)),
+          Expanded(child: Icon(Icons.repeat, size: _iconSize)),
+          Expanded(child: Icon(Icons.favorite, size: _iconSize)),
+          Expanded(child: Icon(Icons.inbox, size: _iconSize)),
+        ]));
+  }
+}
+
+class Tweet extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.only(bottom: 5),
+        child: Row(children: [
+          Image.network(
+            "https://pbs.twimg.com/profile_images/905593281270005760/UkL_D7ls_400x400.jpg",
+            height: 50,
+            fit: BoxFit.cover,
+          ),
+          Expanded(
+            child:
+                Column(children: [Text("unchi"), Text("unchi"), TweetLink()]),
+          ),
+        ]));
   }
 }
